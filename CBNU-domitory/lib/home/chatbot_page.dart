cd50d4b78eec500731c43b585c50d8c73b4cfef7
eSeohyeon <leeseohyeon1203@gmail.com>
2025-08-29 21:37:27 +0900

    fix: 챗봇 주의사항 추가

--- CBNU-domitory/lib/home/chatbot_page.dart
+++ CBNU-domitory/lib/home/chatbot_page.dart
@@ -35,7 +35,7 @@ class _ChatbotPageState extends State<ChatbotPage> {
       _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
     });
 
-    _messages.add({'챗봇' : '저는 충북대학교 생활관과 관련된 질문에만 답변할 수 있어요. 관련되지 않은 질문은 답변드리기 어려운 점 양해 부탁드려요.\n무엇이 궁금하신가요?'});
+    _messages.add({'챗봇' : '저는 충북대학교 생활관과 관련된 질문에만 답변할 수 있어요. 관련되지 않은 질문은 답변드리기 어려운 점 양해 부탁드려요.\n\n-원하는 답변이 나오지 않는다면?\n 1. 질문을 짧고 간단하게 적어주세요.\n 2. 핵심 키워드를 반드시 포함해서 질문해주세요.\n\n무엇이 궁금하신가요?'});
   }
 
   @override
