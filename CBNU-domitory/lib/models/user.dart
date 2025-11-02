bb2ed8364496caf6fffb5ecace8c27b66276976b
kimahhyeon <kah0220@naver.com>
2025-11-03 02:34:36 +0900

    변수명 DB와 동일하게 수정

--- CBNU-domitory/lib/models/user.dart
+++ CBNU-domitory/lib/models/user.dart
@@ -1,14 +1,15 @@
 class User {
+  final String id; //수정
   final String profilePath;
-  final String name;
+  final String nickname;
   final String department;
-  final String yearEnrolled; // 임시로
+  final String enrollYear; // 임시로
   final String birthYear; // 임시로
   final bool isSmoking;
-  final List<Map<String, dynamic>> checklist;
+  final Map<String, dynamic> checklist;
   final double dormScore;
 
   final String dormitory; // 임시로,
 
-  User({required this.profilePath, required this.name, required this.department, required this.yearEnrolled, required this.isSmoking, required this.checklist, this.dormScore = -1, this.birthYear = '0', this.dormitory=' '});
+  User({required this.id, required this.profilePath, required this.nickname, required this.department, required this.enrollYear, required this.isSmoking, required this.checklist, this.dormScore = -1, this.birthYear = '0', this.dormitory=' '});
 }
